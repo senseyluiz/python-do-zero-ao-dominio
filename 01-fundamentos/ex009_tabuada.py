0a7ed4653f7a09b9763d937d869753054ff65a36
Luis Henrique <senseyluiz@gmail.com>
2026-04-16 17:25:15 -0300

    :sparkles: feat(ex007)9R solução do exercicio 009_tabuada

---
+++ 01-fundamentos/ex009_tabuada.py
@@ -0,0 +1,17 @@
+# 009
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+num = int(input("Digite um número inteiro para ver sua tabuada: "))
+msg = f" Tabuada de {num} "
+print(f"{msg}")
+print("="*12)
+print(f"{num} x {1:2} = {num * 1:2}\n"
+      f"{num} x {2:2} = {num * 2}\n"
+      f"{num} x {3:2} = {num * 3}\n"
+      f"{num} x {4:2} = {num * 4}\n"
+      f"{num} x {5:2} = {num * 5}\n"
+      f"{num} x {6:2} = {num * 6}\n"
+      f"{num} x {7:2} = {num * 7}\n"
+      f"{num} x {8:2} = {num * 8}\n"
+      f"{num} x {9:2} = {num * 9}\n"
+      f"{num} x {10} = {num * 10}")
+print("="*12)
