8ea1c23e2c2999663b2d311a2e23937e92252e09
Luis Henrique <senseyluiz@gmail.com>
2026-04-17 22:18:57 -0300

    :sparkles: feat(023) Resolução do exercicio 023__separaÃcao_digito

---
+++ 01-fundamentos/ex023_separacao_digitos.py
@@ -0,0 +1,12 @@
+# 023
+# Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+
+num = int(input("Digite um número: "))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f"Unidade: {u}\n"
+      f"Dezena: {d}\n"
+      f"Centena: {c}\n"
+      f"Milhar: {m}\n")
