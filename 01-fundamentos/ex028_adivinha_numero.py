10ffc093d84532ac28dec4eea1818d89572fcf8d
Luis Henrique <senseyluiz@gmail.com>
2026-04-19 11:04:30 -0300

    :sparkles: feat(028) Adicionando tempo na resolução do exercicio 028_adivinha_numero

--- 01-fundamentos/ex028_adivinha_numero.py
+++ 01-fundamentos/ex028_adivinha_numero.py
@@ -4,10 +4,15 @@
 # O programa deve escrever na tela se o usuário venceu ou perdeu
 
 import random
+from time import sleep
 comp = random.randint(0, 5)
 user = int(input("Escolha um número entre 0 e 5: "))
+print("Processando...")
+sleep(2)
 print(f"O número escolhido pelo computador foi: {comp}")
+sleep(0.5)
 print(f"O número que você escolheu foi: {user}")
+sleep(0.5)
 if user == comp:
     print("Você acertou! PARABÉNS")
 else:
