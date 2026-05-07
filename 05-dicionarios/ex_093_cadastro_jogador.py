a2f82bf13f7fff917a4159d012060239222e2e8d
Luis Henrique <senseyluiz@gmail.com>
2026-05-07 19:03:36 -0300

    :sparkles: feat(095) Resolução do exercicio 095_aprimorando_dicionario

--- 05-dicionarios/ex_093_cadastro_jogador.py
+++ 05-dicionarios/ex_093_cadastro_jogador.py
@@ -7,7 +7,7 @@ partidas = []
 jogador['nome'] = str(input('Nome: '))
 tot = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
 for c in range(0, tot):
-    partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+    partidas.append(int(input(f'Quantos gols na partida {c}? ')))
 
 jogador['gols'] = partidas[:]
 jogador['total'] = sum(partidas)
@@ -17,5 +17,5 @@ for k, v in jogador.items():
 print('-=' * 30)
 print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
 for i, v in enumerate(jogador["gols"]):
-    print(f"  => Na partida {i+1} fez {v} gols.")
+    print(f"  => Na partida {i} fez {v} gols.")
 print(f'Foi um total de {jogador["total"]} gols.')
