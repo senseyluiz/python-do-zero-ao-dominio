eae6de5dacf46ca15ddf08316e9ded7bd7f893ff
Luis Henrique <senseyluiz@gmail.com>
2026-05-13 16:24:44 -0300

    :sparkles: feat(110) Resolução do exercicio 110_moeda4

--- utils/moeda.py
+++ utils/moeda.py
@@ -15,3 +15,13 @@ def moeda(valor, formato=True):
         return f"R${valor:.2f}".replace('.', ',')
     else:
         return valor
+def resumo(valor=0, aumento=10, reducao=5):
+    print("-" * 40)
+    print(f"{'RESUMO DO VALOR'.center(40)}")
+    print("-" * 40)
+    print(f"{'PREÇO ANALISADO:':<25}{moeda(valor):<12}\n"
+          f"{'DOBRO DO PREÇO:':<25}{moeda(dobro(valor)):<12}\n"
+          f"{'METADE DO PREÇO:':<25}{moeda(metade(valor)):<12}\n"
+          f"{f'{aumento}% DE AUMENTO:':<25}{moeda(aumentar(valor, aumento)):<12}\n"
+          f"{f'{reducao}% DE REDUÇÃO:':<25}{moeda(diminuir(valor, reducao)):<12}")
+    print("-" * 40)
