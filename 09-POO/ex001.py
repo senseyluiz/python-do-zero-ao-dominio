5b066a9c119e7bb9936c48e147230eb08d492c27
Luis Henrique <senseyluiz@gmail.com>
2026-05-25 17:36:49 -0300

    Primeiro exercicio de POO

---
+++ 09-POO/ex001.py
@@ -0,0 +1,16 @@
+class Gafanhoto:
+    def __init__(self):
+        self.nome = ''
+        self.idade = 0
+
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é um gafanhoto e tem {self.idade} anos"
+
+g1 = Gafanhoto()
+g1.nome = "Luis Henrique"
+g1.idade = 43
+g1.aniversario()
+print(g1.mensagem())
