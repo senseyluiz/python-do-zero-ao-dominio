a2bca98ee63648c576084c469f4a81e4c3842e45
Luis Henrique <senseyluiz@gmail.com>
2026-05-27 20:21:08 -0300

    Exercicio 003 de POO

---
+++ 09-POO/ex003.py
@@ -0,0 +1,27 @@
+class ContaBancaria:
+    """
+    Essa classe cria uma conta bancária com id, nome e saldo e permite fazer saque e depósitos
+    """
+    def __init__(self, id, nome, saldo=0):
+        self.id = id
+        self.nome = nome
+        self.saldo = saldo
+
+    def __str__(self):
+        return f"A conta de {self.nome} tem R${self.saldo:,.2f} reais"
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"Depósito de R${valor:.2f} reais realizado com \33[32mSUCESSO\33[0m")
+
+    def sacar(self, valor):
+        if self.saldo <= valor:
+            print ("Saldo insuficiente!!!")
+        else:
+            self.saldo -= valor
+
+
+
+luis = ContaBancaria(110, "Luis", 5000)
+print(luis)
+luis.depositar(1500)
