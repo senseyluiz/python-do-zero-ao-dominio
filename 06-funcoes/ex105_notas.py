dd19fd814b90e95935fde3ef6623be6ee34bca99
Luis Henrique <senseyluiz@gmail.com>
2026-06-14 19:47:27 -0300

    Atualiza

--- 06-funcoes/ex105_notas.py
+++ 06-funcoes/ex105_notas.py
@@ -12,7 +12,7 @@ def notas(*notas, sit=False):
     — > Função para analisar notas e situação de vários alunos,
     :param notas: uma ou mais notas dos alunos (aceita várias)
     :param sit: valor opcional, indicando se deve ou não adicionar a situação
-    :return: dicionário com várias informações sobre a situação da turma
+    :return: dicionário com várias informações sobre a situação da turma 
     """
     global situacao
     total = len(notas)
